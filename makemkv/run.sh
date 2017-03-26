b5ca039b6604f78808b6a9ffc6918f016a99ef73
Ian Martin <ian@imartin.net>
2017-03-25 22:14:20 -0600

    Add makemkv and dvdbackup dockerfiles

---
+++ makemkv/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+KEY=$(curl "http://www.makemkv.com/forum2/viewtopic.php?f=5&t=1053" -s | awk 'FNR == 243 {print $57}' | cut -c 21-88)
+
+mkdir -p ${HOME}/.MakeMKV
+
+echo "app_Key = \"${KEY}\"" > ${HOME}/.MakeMKV/settings.conf
+
+set +x
+echo "makemkvcon backup --decrypt $@"
+makemkvcon backup --decrypt $@
